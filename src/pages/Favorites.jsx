6e4d61437c3690e35e2f55ef6cdd268ceb9fa587
Kim Guerrette <kguerrette@gmail.com>
2025-03-31 11:35:58 -0700

    Initial Commit. Tech with Tim tutorial through routing, before CSS Styling.

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,10 @@
+function Favorites() {
+  return (
+    <div className="favorites-empty">
+      <h2>No Favorite Movies Yet</h2>
+      <p>Start adding movies to your favorites and they will appear here.</p>
+    </div>
+  );
+}
+
+export default Favorites;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import MovieCard from "../components/MovieCard";
+import { useState } from "react";
+
+function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const movies = [
+    { id: 1, title: "John Wick", release_date: "2014" },
+    { id: 2, title: "Terminator", release_date: "1984" },
+    { id: 3, title: "The Matrix", release_date: "1999" },
+  ];
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("");
+  };
+
+  return (
+    <div className="home">
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search for movies ..."
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
+
+      <div className="movies-grid">
+        {movies.map(
+          (movie) =>
+            movie.title.toLowerCase().startsWith(searchQuery.toLowerCase()) && (
+              <MovieCard movie={movie} key={movie.id} />
+            )
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
